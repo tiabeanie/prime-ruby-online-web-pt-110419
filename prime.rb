728075c5b3eba47eb68e7bfe4b66ded85d575645
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-12 17:24:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,13 @@
-def prime?(num)
-  sieve(num).include?(num)
+def prime?(number)
+  if number <= 1 || number == 0 || number == 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true  # <= need explanation
 end
   
